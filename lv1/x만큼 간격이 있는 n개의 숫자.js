8503e2b5ecb5ffc59a891b9a54145dd88d588fca
sehyun <e6d1fe@gmail.com>
2023-07-28 11:16:41 +0900

    lv1: 12954

---
+++ lv1/x만큼 간격이 있는 n개의 숫자.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12954
+
+function solution(x, n) {
+  const answer = [];
+
+  for (let i = 1; i <= n; i++) {
+    answer.push(x * i);
+  }
+
+  return answer;
+}
