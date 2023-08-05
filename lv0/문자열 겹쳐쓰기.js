5dec162892f27765693d1edfe258e612f04e1d72
sehyun <e6d1fe@gmail.com>
2023-08-05 23:43:06 +0900

    lv0: 181943

---
+++ lv0/문자열 겹쳐쓰기.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181943
+
+function solution(my_string, overwrite_string, s) {
+  return my_string.slice(0, s) + overwrite_string + my_string.slice(s + overwrite_string.length);
+}
