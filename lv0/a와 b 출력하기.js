b81e424fd318dbbb417a1bdc8907ba84a1ec45b5
sehyun <e6d1fe@gmail.com>
2023-08-07 21:44:17 +0900

    lv0: 181951

---
+++ lv0/a와 b 출력하기.js
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181951
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  console.log(`a = ${input[0]}`);
+  console.log(`b = ${input[1]}`);
+});
