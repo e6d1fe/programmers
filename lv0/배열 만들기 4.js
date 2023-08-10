fd8c400a5f1e5090d9cfe1c69c9f5e4090df0c20
sehyun <e6d1fe@gmail.com>
2023-08-10 16:49:15 +0900

    lv0: 181918

---
+++ lv0/배열 만들기 4.js
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181918
+
+function solution(arr) {
+  const stk = [];
+
+  let i = 0;
+
+  while (i < arr.length) {
+    if (stk.length === 0) {
+      stk.push(arr[i]);
+      i += 1;
+    } else if (stk.length !== 0 && stk[stk.length - 1] < arr[i]) {
+      stk.push(arr[i]);
+      i += 1;
+    } else if (stk.length !== 0 && stk[stk.length - 1] >= arr[i]) {
+      stk.pop();
+    }
+  }
+
+  return stk;
+}
