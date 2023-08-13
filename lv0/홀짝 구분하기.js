5430475c647b6b184f8bac7914b05707425d7fb9
sehyun <e6d1fe@gmail.com>
2023-08-13 23:55:43 +0900

    lv0: 181944

---
+++ lv0/홀짝 구분하기.js
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181944
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  n = Number(input[0]);
+  console.log(n % 2 ? `${n} is odd` : `${n} is even`);
+});
