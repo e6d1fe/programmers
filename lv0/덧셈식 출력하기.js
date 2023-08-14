cb3e2f834462028b28f5c660ad3f29db31a53e2b
sehyun <e6d1fe@gmail.com>
2023-08-14 14:37:32 +0900

    lv0: 181947

---
+++ lv0/덧셈식 출력하기.js
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181947
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  const [a, b] = input.map(Number);
+
+  console.log(`${a} + ${b} = ${a + b}`);
+});
