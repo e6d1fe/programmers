b98e780e65eeb125e681a686a7b6861c4d70caf0
sehyun <e6d1fe@gmail.com>
2023-08-14 14:20:24 +0900

    lv0: 120823

---
+++ lv0/직각삼각형 출력하기.js
@@ -0,0 +1,23 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120823
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  const width = Number(input[0]);
+
+  let triangle = "";
+
+  for (let i = 1; i <= width; i++) {
+    triangle += "*".repeat(i) + "\n";
+  }
+
+  console.log(triangle);
+});
