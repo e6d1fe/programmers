b15b5ab4d07217a343b5ef525575187223fc0a89
sehyun <e6d1fe@gmail.com>
2023-08-19 03:08:47 +0900

    lv1: 147355

---
+++ lv1/크기가 작은 부분 문자열.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/147355
+
+function solution(t, p) {
+  let result = 0;
+
+  for (let i = 0; i <= t.length - p.length; i++) {
+    if (Number(t.slice(i, i + p.length)) <= Number(p)) result += 1;
+  }
+
+  return result;
+}
